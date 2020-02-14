889938bfab6de4c34730247e6e5faea046c1aea6
Chetan77 <cchoudhari1@gmail.com>
2020-02-14 14:37:06 +0530

    [Chetan]Add. added implementation of board disply

--- SnakeLadderSimulator.sh
+++ SnakeLadderSimulator.sh
@@ -1,1 +1,25 @@
-echo "WelCome to Snake and Ladder Simulator"
+echo "start the game"
+a1=100
+b1=91
+echo "======================================================="
+for (( i = 1; i < 11; i++ )); do
+	echo -n "|| "
+	a=$a1
+	b=$b1
+	for (( j = 1; j < 11; j++ )); do
+		if (($(($i%2))==1)); then
+		echo -n " $a |"
+		a=$(($a-1))
+
+		elif (($(($i%2))==0)); then
+		echo -n " $b |"
+		b=$(($b+1))
+		
+		fi
+
+	done
+	echo "|"
+echo "======================================================="
+	a1=$(($a1-10))
+	b1=$(($b1-10))
+done
