5a3c47fc979da23534c005752f5c7b069258ab31
Chetan77 <cchoudhari1@gmail.com>
2020-02-14 15:16:28 +0530

    [Chetan]Add. added useCase1-player at zero position

--- SnakeLadderSimulator.sh
+++ SnakeLadderSimulator.sh
@@ -1,2 +1,34 @@
-#!/bin/bash
-echo "WelCome to Snake and Ladder Simulator"
+function PlayerAtZero(){
+	echo "enter player name"
+	read name
+	Display
+	echo "player here '$name'"
+}
+function Display(){
+	a1=100
+	b1=91
+	declare -A ladder
+	echo "======================================================="
+	for (( i = 1; i < 11; i++ )); do
+		echo -n "|| "
+		a=$a1
+		b=$b1
+		for (( j = 1; j < 11; j++ )); do
+			if (($(($i%2))==1)); then
+				echo -n " $a |"
+				ladder[$i,$j]=$a
+				a=$(($a-1))
+
+			elif (($(($i%2))==0)); then
+				echo -n " $b |"
+				ladder[$i,$j]=$a
+				b=$(($b+1))
+			fi
+		done
+		echo "|"
+		echo "======================================================="
+		a1=$(($a1-10))
+		b1=$(($b1-10))
+	done
+}
+PlayerAtZero
